25016c7dcf7972a6483f430b891ab21260fb5141
sherlynkek <sherlynkek00@gmail.com>
2024-11-20 00:08:21 +0800

    main

---
+++ src/test/java/sg/edu/nus/iss/vttp5_day2w/Vttp5Day2wApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5_day2w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5Day2wApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
