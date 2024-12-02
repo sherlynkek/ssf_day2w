f34a7912e293cbb1a6b484c94716f4a9fbe84f8b
sherlynkek <sherlynkek00@gmail.com>
2024-12-02 17:18:53 +0800

    Repo Comments

--- src/main/java/sg/edu/nus/iss/vttp5_day2w/repo/NumberGeneratorRepo.java
+++ src/main/java/sg/edu/nus/iss/vttp5_day2w/repo/NumberGeneratorRepo.java
@@ -8,6 +8,11 @@ import org.springframework.stereotype.Repository;
 import sg.edu.nus.iss.vttp5_day2w.model.NumberGenerator;
 
 @Repository
+// Input: Takes numsToGenerate as the number of random numbers (images) to generate
+// Logic:
+// Creates a new instance of the NumberGenerator class, passing numsToGenerate as the parameter
+// Calls the generateNums() method on the NumberGenerator instance to produce the list of numbers
+// Output: Returns the generated list of random numbers (as file names)
 public class NumberGeneratorRepo {
     public List<String> getNumbers(int numsToGenerate) throws IOException{
         return new NumberGenerator(numsToGenerate).generateNums();
